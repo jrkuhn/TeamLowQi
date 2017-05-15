3b51460889976327ba547aa3e23a224bdfb6c770
Justin Kuhn <jrkuhn@iastate.edu>
2017-05-15 17:53:47 -0500

    updated Calculator

--- LowQiCalc.js
+++ LowQiCalc.js
@@ -1,6 +1,7 @@
 //Team Low-Qi Calculator
 
 var Calc = {};
+
 Calc.log = [];
 
 
@@ -8,101 +9,179 @@ Calc.log = [];
 $(document).ready(function() {
     Calc.init();
 
-
+    Calc.LP1 = $("#LP1").html();
+    Calc.LP2 = $("#LP2").html();
+    Calc.T1 = $("#temp1").html();
+    Calc.T2 = $("#temp2").html();
 
     $('body').click(function(e){
         var action = e.target.id;
+        var num1 = parseInt($('#temp1').html());
+        var num2 = parseInt($('#temp2').html());
 
         switch(action){
             case 'plus1':
+                var numT1 = parseInt($('#temp1').html());
+                var LP1 = parseInt($('#LP1').html());
+                if(numT1 >= 0)
+                    $('#LP1').html(LP1 + numT1);
+                if(LP1 + numT1 > 9999) {
+                    $('#LP1').css('font-size', 46);
+                    $('#LP1').css({"padding":"24px "});
+                }
+                $('#pop1').hide();
+                $('#temp1').html("");
                 break;
+
             case 'minus1':
+                var numT1 = parseInt($('#temp1').html());
+                var LP1 = parseInt($('#LP1').html());
+                if(numT1 >= 0)
+                    $('#LP1').html(LP1 - numT1);
+
+                if(LP1 - numT1 < 0)
+                    $('#LP1').html(0);
+
+                $('#pop1').hide();
+                $('#temp1').html("");
                 break;
+
             case 'plus2':
+                var numT2 = parseInt($('#temp2').html());
+                var LP2 = parseInt($('#LP2').html());
+                if(numT2 >= 0)
+                    $('#LP2').html(LP2 + numT2);
+                if(LP2 + numT2 > 9999) {
+                    $('#LP2').css('font-size', 46);
+                    $('#LP2').css({"padding": "24px "});
+                }
+                $('#pop2').hide();
+                $('#temp2').html("");
                 break;
+
             case 'minus2':
+                var numT2 = parseInt($('#temp2').html());
+                var LP2 = parseInt($('#LP2').html());
+                if(numT2 >= 0)
+                    $('#LP2').html(LP2 - numT2);
+
+                if(LP2 - numT2 < 0)
+                    $('#LP2').html(0);
+
+                $('#pop2').hide();
+                $('#temp2').html("");
                 break;
+
             case 'LP1':
                 $('#pop1').show();
-                break;
-            case 'LP2':
-                $('#pop2').show();
-                break;
-            case 'reset':
-                alert("reset");
-                Calc.lp1 = 8000;
-                Calc.lp2 = 8000;
-                Calc.temp1 = 0;
-                Calc.temp2 = 0;
-                break;
-            case 'coin':
-                alert("coin");
-                break;
-            case 'dice':
-                alert("dice");
-                break;
-            case 'close1':
-                $('#pop1').hide();
-                break;
-            case 'close2':
                 $('#pop2').hide();
                 break;
 
+            case 'LP2':
+                $('#pop2').show();
+                $('#pop1').hide();
+                break;
+            case 'reset':
+                $('#LP1').html(8000);
+                $('#LP2').html(8000);
+                $('#temp1').html("");
+                $('#temp2').html("");
+                break;
+
+            case 'coin':
+                alert("coin");
+                break;
+
+            case 'dice':
+                alert("dice");
+                break;
+
+            case 'close1':
+                $('#pop1').hide();
+                $('#temp1').html("");
+                break;
+
+            case 'close2':
+                $('#pop2').hide();
+                $('#temp2').html("");
+                break;
+
             case 'pop1num1':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num1").value);
                 break;
             case 'pop1num2':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num2").value);
                 break;
             case 'pop1num3':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num3").value);
                 break;
             case 'pop1num4':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num4").value);
                 break;
             case 'pop1num5':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num5").value);
                 break;
             case 'pop1num6':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num6").value);
                 break;
             case 'pop1num7':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num7").value);
                 break;
             case 'pop1num8':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num8").value);
                 break;
             case 'pop1num9':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num9").value);
                 break;
             case 'pop1num0':
-                break;
-            case 'enter1':
+                $('#temp1').html( $('#temp1').html() + document.getElementById("pop1num0").value);
                 break;
 
             case 'pop2num1':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num1").value);
                 break;
             case 'pop2num2':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num2").value);
                 break;
             case 'pop2num3':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num3").value);
                 break;
             case 'pop2num4':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num4").value);
                 break;
             case 'pop2num5':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num5").value);
                 break;
             case 'pop2num6':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num6").value);
                 break;
             case 'pop2num7':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num7").value);
                 break;
             case 'pop2num8':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num8").value);
                 break;
             case 'pop2num9':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num9").value);
                 break;
             case 'pop2num0':
+                $('#temp2').html( $('#temp2').html() + document.getElementById("pop2num0").value);
                 break;
-            case 'enter2':
-                break;
+
             case 'pop1':
                 break;
             case 'pop2':
+                break;
+            case 'temp1':
+                break;
+            case 'temp2':
                 break;
 
             default:
                 $('#pop1').hide();
                 $('#pop2').hide();
-                Calc.temp1 = 0;
-                Calc.temp2 = 0;
+                $('#temp1').html("");
+                $('#temp2').html("");
         }
 
     });
@@ -111,14 +190,6 @@ $(document).ready(function() {
 
 
 Calc.init = function(){
-    Calc.lp1 = 8000;
-    Calc.lp2 = 8000;
 
-    Calc.p1total = 0;
-    Calc.p1First = 0;
-    Calc.p1Second = 0;
-
-    Calc.p2First = 0;
-    Calc.p2Second = 0;
 
 };
